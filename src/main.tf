4e3688c6503a6ce0324e875d6cd9a56ba4b9cbb3
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-01-30 16:52:26 +0530

    Add files via upload

---
+++ src/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "tf-state" {
+  source      = "./modules/tf-state"
+  bucket_name = "cc-tf-state-backend-ci-cd-demo023"
+}
+
+module "vpc-infra" {
+  source = "./modules/vpc"
+
+  # VPC Input Vars
+  vpc_cidr             = local.vpc_cidr
+  availability_zones   = local.availability_zones
+  public_subnet_cidrs  = local.public_subnet_cidrs
+  private_subnet_cidrs = local.private_subnet_cidrs
+}
